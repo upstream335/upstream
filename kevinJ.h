1510f16d9c1d498959b26f000d2e95dc653e9eb0
Kevin <kjenkin3737@yahoo.com>
2016-05-02 11:47:09 -0700

    Update kevinJ.h

--- kevinJ.h
+++ kevinJ.h
@@ -1,21 +1,9 @@
 #ifndef _SOUND_H_
 #define _SOUND_H_
 
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <unistd.h>
-#include <cstring>
-#include <stdio.h>
-#include <cmath>
-#include <X11/Xlib.h>
-#include <X11/keysym.h>
-#include <GL/glx.h>
-#include <stdlib.h>
 #include <AL/al.h>
 #include <AL/alut.h>
 #include "game.h"
-
 
 extern void playSounds(const char * sound, float gain, bool loop, bool muted);
 extern void stopMusic(void); 
